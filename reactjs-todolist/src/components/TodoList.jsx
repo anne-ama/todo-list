890ea746ec5daae917debc50bdab6bd22e15feb8
Anne Agda <anne.agda1@gmail.com>
2024-07-24 11:24:35 +1000

    built some todo cards and input buttons (functionless)

--- reactjs-todolist/src/components/TodoList.jsx
+++ reactjs-todolist/src/components/TodoList.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
+    let todos = [
+        'Go to the gym',
+        'Pick up the kids from school',
+        'Eat more fruits'
+    ]
   return (
-    <div>TodoList</div>
+    <ul className='main'>
+        {todos.map((todo, todoIndex) =>{
+            return(
+                <TodoCard key={todoIndex}>
+                    <p>{todo}</p>
+                </TodoCard>
+            )
+        })}
+    </ul>
   )
 }
