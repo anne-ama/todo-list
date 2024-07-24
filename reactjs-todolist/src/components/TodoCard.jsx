9f5afaaa8edf7a4ad9ccf7c4c6eb1da86c11544b
Anne Agda <anne.agda1@gmail.com>
2024-07-24 11:29:33 +1000

    fixed bugs concerning font imports and card spacing

--- reactjs-todolist/src/components/TodoCard.jsx
+++ reactjs-todolist/src/components/TodoCard.jsx
@@ -4,8 +4,8 @@ export default function TodoCard(props) {
     const {children} = props
   return (
     <li className='todoItem'>
+        {children}
         <div className='actionsContainer'>
-            {children}
             <i className="fa-solid fa-pen-to-square"></i>
             <i className="fa-regular fa-trash-can"></i>
         </div>
