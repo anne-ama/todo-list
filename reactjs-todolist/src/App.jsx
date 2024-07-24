b6d79b7bec1ad33b1a4d0ee521a729354a6ab64a
Anne Agda <anne.agda1@gmail.com>
2024-07-24 11:47:38 +1000

    added functionality to the TodoInput

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    'Go to the gym',
+    'Pick up the kids from school',
+    'Eat more fruits'
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
   return (
     <>
-      <TodoInput />
-      <TodoList />
+      <TodoInput handleAddTodo={handleAddTodos} />
+      <TodoList todos={todos} />
     </>
   )
 }
--- reactjs-todolist/src/components/TodoList.jsx
+++ reactjs-todolist/src/components/TodoList.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
-    let todos = [
-        'Go to the gym',
-        'Pick up the kids from school',
-        'Eat more fruits'
-    ]
-  return (
-    <ul className='main'>
-        {todos.map((todo, todoIndex) =>{
-            return(
-                <TodoCard key={todoIndex}>
-                    <p>{todo}</p>
-                </TodoCard>
-            )
-        })}
-    </ul>
-  )
+export default function TodoList(props) {
+    const {todos} = props
+    return (
+        <ul className='main'>
+            {todos.map((todo, todoIndex) =>{
+                return(
+                    <TodoCard key={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                )
+            })}
+        </ul>
+    )
 }
